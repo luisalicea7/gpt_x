f901930a9d5c19074d460692c703f2ea9bb5382f
Luis Alicea <luis.alicea7@upr.edu>
2023-08-30 05:53:36 -0400

    Render chat sidebar

--- components/ChatSidebar/ChatSidebar.tsx
+++ components/ChatSidebar/ChatSidebar.tsx
@@ -1,9 +1,12 @@
 import { UserButton, useAuth } from "@clerk/nextjs";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextApiRequest } from "next";
-import { useEffect } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { json } from "stream/consumers";
 
 export const ChatSidebar = () => {
+  const [chatList, setChatList] = useState([]);
   useEffect(() => {
     const loadChats = async () => {
       const res = await fetch("/api/chat/getChatList", {
@@ -11,12 +14,21 @@ export const ChatSidebar = () => {
       });
       const data = await res.json();
       console.log("CHAT LIST: ", data);
+      setChatList(data?.chats);
     };
     loadChats();
   }, []);
 
   return (
-    <div className="bg-[#292929]">
+    <div className="flex flex-col overflow-hidden text-white bg-[#292929]">
+      <Link href="/chat">Create Chat</Link>
+      <div className="flex-1 overflow-auto bg-[#1F1F1F]">
+        {chatList.map((chat: any) => (
+          <Link key={chat._id} href={`/chat/${chat._id}`}>
+            {chat.title}
+          </Link>
+        ))}
+      </div>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
