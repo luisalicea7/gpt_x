da8b6610100686bee60b512e31d8cc1a54ddc8ed
Luis Alicea <luis.alicea7@upr.edu>
2023-09-01 04:35:07 -0400

    More chatSidebar styling

--- components/ChatSidebar/ChatSidebar.tsx
+++ components/ChatSidebar/ChatSidebar.tsx
@@ -1,11 +1,8 @@
 import { UserButton, useAuth } from "@clerk/nextjs";
-import { getAuth } from "@clerk/nextjs/server";
 import { faMessage, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { NextApiRequest } from "next";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { json } from "stream/consumers";
 
 export const ChatSidebar = () => {
   const [chatList, setChatList] = useState([]);
@@ -23,7 +20,7 @@ export const ChatSidebar = () => {
 
   return (
     <div className="flex flex-col overflow-hidden text-white bg-[#292929]">
-      <Link className="side-menu-item" href="/chat">
+      <Link className="new-chat-btn" href="/chat">
         <FontAwesomeIcon icon={faPlus} /> Create Chat
       </Link>
       <div className="flex-1 overflow-auto bg-[#1F1F1F]">
@@ -36,7 +33,7 @@ export const ChatSidebar = () => {
           </Link>
         ))}
       </div>
-      <div className="p-4 flex justify-center items-center gap-2">
+      <div className="p-3 flex justify-center items-center gap-2">
         <UserButton afterSignOutUrl="/" />
       </div>
     </div>
