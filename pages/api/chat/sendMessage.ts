5bcecdc5d346611190037dd703da6f0c2ab36160
Luis Alicea <luis.alicea7@upr.edu>
2023-09-01 04:51:23 -0400

    Finished adding create-a-chat endpoint

--- pages/api/chat/sendMessage.ts
+++ pages/api/chat/sendMessage.ts
@@ -10,17 +10,25 @@ export default async function handler(req: Request, res: NextApiResponse) {
     try {
         const { message } = await req.json()
 
-        const res = await fetch("/api/chat/createNewChat", {
+        const customInitialMessage = {
+            role: "system",
+            content: ""
+        }
+
+        const res = await fetch(`${req.headers.get("origin")}/api/chat/createNewChat`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
+              cookie: req.headers.get("cookie") || "",
             },
             body: JSON.stringify({ message }),
           });
       
           const data = await res.json();
           console.log("NEW CHAT: ", data);
-          
+          const chatId = data._id;
+
+
         const stream = await OpenAIEdgeStream(
             "https://api.openai.com/v1/chat/completions", {
                 headers: {
@@ -33,6 +41,10 @@ export default async function handler(req: Request, res: NextApiResponse) {
                     messages: [{content: message, role: "user"}],
                     stream: true
                 })
+        }, {
+            onAfterStream: async ({fullContent}) => {
+
+            }
         }
         )
         return new Response(stream)
