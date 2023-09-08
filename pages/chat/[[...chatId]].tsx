0f7027bc6d15a89e1f85e190d64a905fe08aa2ec
Luis Alicea <luis.alicea7@upr.edu>
2023-09-08 01:15:50 -0400

    Redirect to newchatId route if new chat

--- pages/chat/[[...chatId]].tsx
+++ pages/chat/[[...chatId]].tsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import { ChatSidebar, Message } from "../../components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { streamReader } from "openai-edge-stream";
 import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/router";
 
 export default function ChatPage() {
   const [messageText, setMessageText] = useState("");
@@ -15,6 +16,16 @@ export default function ChatPage() {
     }[]
   >([]);
   const [generatingResponse, setGeneratingResponse] = useState(false);
+  const [newChatId, setNewChatId] = useState<string | null>(null);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!generatingResponse && newChatId) {
+      setNewChatId(null);
+      router.push(`/chat/${newChatId}`);
+    }
+  }, [newChatId, generatingResponse, router]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -47,9 +58,14 @@ export default function ChatPage() {
 
     const reader = data.getReader();
     await streamReader(reader, (message) => {
-      console.log(message);
-      setIncomingMessage((s) => `${s}${message.content}`);
+      console.log("Message: ", message);
+      if (message.event === "newChatId") {
+        setNewChatId(message.content);
+      } else {
+        setIncomingMessage((s) => `${s}${message.content}`);
+      }
     });
+
     setGeneratingResponse(false);
   };
 
